aa98fc1187f01b07fd54dca32e4c7e1c62bfcec6
Shawn Rhodes <shawn@massivelines.com>
2018-05-31 03:06:15 -0500

    Index page almost complete, working on membership section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function () {
+  var menuButton = document.getElementById('menuButton');
+  var overlay = document.querySelector('div.overlay');
+  var closeText = document.getElementById('closeText');
+  var menuText = document.getElementById('menuText');
+
+  function toggleOpen() {
+    menuButton.classList.toggle('open');
+    overlay.classList.toggle('open');
+    menuText.classList.toggle('open');
+    closeText.classList.toggle('open');
+
+  }
+  
+  menuButton.addEventListener('click', toggleOpen);
+})();
