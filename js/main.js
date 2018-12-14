7bd33232ae9ccde5c1fe375f0f7295bbe40727ae
Shawn Rhodes <shawn@massivelines.com>
2018-12-14 11:21:58 -0600

    Testing header image fade in and out

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   var menuButton = document.getElementById('menuButton');
   var overlay = document.querySelector('div.overlay');
   var closeText = document.getElementById('closeText');
@@ -9,10 +9,47 @@
     overlay.classList.toggle('open');
     menuText.classList.toggle('open');
     closeText.classList.toggle('open');
-
   }
-  
+
   menuButton.addEventListener('click', toggleOpen);
   closeText.addEventListener('click', toggleOpen);
   menuText.addEventListener('click', toggleOpen);
 })();
+
+var bgImageArray = [
+  '../img/header/1.jpg',
+  '../img/header/2.jpg',
+  '../img/header/3.jpg',
+  '../img/header/4.jpg',
+];
+//   secs = 4;
+bgImageArray.forEach(function(img) {
+  new Image().src = img;
+  // caches images, avoiding white flash between background replacements
+});
+
+var headerBgElm = document.getElementById('headerBg');
+var previousHeaderBgElm = document.getElementById('previousHeaderBg');
+previousHeaderBgElm.classList.add('bgImage1');
+
+var backgroundImage = 1;
+var fadeInToggle = false;
+
+function fadeHeaderImages(callback) {
+  // setInterval(function() {
+  //   backgroundImage++;
+  //   headerBgElm.classList.add('bgImage'+backgroundImage);
+  //   headerBgElm.classList.toggle('fadeIn');
+  //   fadeInToggle = !fadeInToggle;
+  //   console.log('setInterval')
+  //   callback();
+  // }, 6000);
+}
+
+fadeHeaderImages(function(){
+//  console.log('callback');
+//  previousHeaderBgElm.classList.remove('bgImage'+backgroundImage);
+//  previousHeaderBgElm.classList.add('bgImage'+backgroundImage);
+})
+
+
