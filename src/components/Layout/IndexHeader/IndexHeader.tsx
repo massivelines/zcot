177cf6fb51f68b597f642aa55c549cd0f8dbc229
Shawn Rhodes <shawn@massivelines.com>
2019-01-08 01:24:52 -0600

    Fading header background image

--- src/components/Layout/IndexHeader/IndexHeader.tsx
+++ src/components/Layout/IndexHeader/IndexHeader.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 
 // TODO: make only one header, height controlled page or index
 
@@ -7,11 +7,34 @@ import Overlay from './Overlay';
 
 import './IndexHeader.scss';
 
-const bgImagesArr = ['../../../images/header-bg/1.jpg'];
+import bgImage1 from '../../../images/header/1.jpg';
+import bgImage2 from '../../../images/header/2.jpg';
+import bgImage3 from '../../../images/header/3.jpg';
+import bgImage4 from '../../../images/header/4.jpg';
+
+const bgImageArr = [bgImage1, bgImage2, bgImage3, bgImage4];
+
+// returns a random number for the header background different from the previous 2
+const randomNum = (
+  length: number,
+  notEqual1?: number,
+  notEqual0?: number,
+): number => {
+  let holdNumber = Math.floor(Math.random() * length);
+  if (holdNumber == notEqual0 || holdNumber == notEqual1) {
+    do {
+      holdNumber = Math.floor(Math.random() * length);
+    } while (holdNumber == notEqual0 || holdNumber == notEqual1);
+  }
+  return holdNumber;
+};
 
 interface IndexHeaderProps {}
 interface IndexHeaderState {
   menuIsOpen: boolean;
+  image0: any;
+  image1: any;
+  imageOp: any;
 }
 
 // const IndexHeader: React.SFC<IndexHeaderProps> = () => (
@@ -20,10 +43,60 @@ class IndexHeader extends Component<IndexHeaderProps, IndexHeaderState> {
     super(props);
 
     this.openMenuClickHandler = this.openMenuClickHandler.bind(this);
+    this.updateHeaderBg = this.updateHeaderBg.bind(this);
 
     this.state = {
       menuIsOpen: false,
+      image1: 1,
+      image0: 0,
+      imageOp: 1,
     };
+  }
+
+  updateHeaderBg() {
+    if (this.state.imageOp === 1) {
+      this.setState({
+        image1: randomNum(
+          bgImageArr.length,
+          this.state.image0,
+          this.state.image1,
+        ),
+      });
+      this.setState({ imageOp: 0 });
+    } else {
+      this.setState({
+        image0: randomNum(
+          bgImageArr.length,
+          this.state.image1,
+          this.state.image0,
+        ),
+      });
+      this.setState({ imageOp: 1 });
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      image1: randomNum(
+        bgImageArr.length,
+        this.state.image0,
+        this.state.image1,
+      ),
+    });
+    this.setState({
+      image0: randomNum(
+        bgImageArr.length,
+        this.state.image0,
+        this.state.image1,
+      ),
+    });
+    // console.log(this.state.image1, this.state.image0);
+    this.interval = setInterval(this.updateHeaderBg, 9000);
+  }
+
+  componentWillUnmount() {
+    // Clear the interval right before component unmount
+    clearInterval(this.interval);
   }
 
   openMenuClickHandler() {
@@ -35,9 +108,16 @@ class IndexHeader extends Component<IndexHeaderProps, IndexHeaderState> {
       <div className="index-header">
         <div
           className="background-image"
-          style={{ backgroundImage: 'url(' + bgImagesArr[0] + ')' }}
+          style={{ backgroundImage: `url(${bgImageArr[this.state.image1]})` }}
         />
-        <div className="new-background-image" />
+        <div
+          className="new-background-image"
+          style={{
+            backgroundImage: `url(${bgImageArr[this.state.image0]})`,
+            opacity: this.state.imageOp,
+            transition: 'opacity 2s ease-in-out',
+          }}
+        />
         <div className="grid-container">
           <div className="left-section darken" />
           <div className="darken left-line first-spacer logo-holder">
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -6,3 +6,7 @@
 
 // And to shim assets, use (one file extension per `declare`):
 // declare module "*.png"
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg';
